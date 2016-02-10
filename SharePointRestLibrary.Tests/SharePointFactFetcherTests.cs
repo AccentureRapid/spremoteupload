e15b63537c390f52acd094509779ee133e7fe4c7
RogerB <roger.boone@gmail.com>
2016-02-09 23:10:19 -0800

    REST calls to read library files and get count of library included here.   Next will be upload file with mapping.

---
+++ SharePointRestLibrary.Tests/SharePointFactFetcherTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharePointRestLibrary.SharePoint;
+using System.Collections.Generic;
+
+namespace SharePointRestLibrary.Tests
+{
+    [TestClass]
+    public class SharePointFactFetcherTests
+    {
+        private string _testUserId = "baaqmd-dev\\sp_farm";
+        private string _testPassword = "P@$$word1!";
+        private string _testBaseUrl = "http://baaqmd-dev.cloudapp.net/sites/RecordCenter";
+
+        [TestMethod]
+        public void GetFileCountByLocationTest()
+        {
+            //arrange
+            ISharePointFactFetcher fetcher = new SharePointFactFetcher (
+                _testUserId,
+                _testPassword,
+                _testBaseUrl
+            );
+            
+            //act
+            var count = fetcher.GetFileCountByLocation("General Ledger");
+
+            //assert
+            Assert.AreEqual(count, 4);
+        }
+
+        [TestMethod]
+        public void GetFilesByLocationTest()
+        {
+            //arrange
+            ISharePointFactFetcher fetcher = new SharePointFactFetcher(
+                _testUserId,
+                _testPassword,
+                _testBaseUrl
+            );
+
+            //act
+            List<string> fileList = new List<string>(fetcher.GetFilesByLocation("General Ledger"));
+
+            //assert
+            Assert.AreEqual(fileList.Count, 4);
+        }
+    }
+}
---
+++ SharePointRestLibrary/SharePoint/SharePointFactFetcher.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePointRestLibrary.SharePoint
+{
+    public class SharePointFactFetcher : ISharePointFactFetcher
+    {
+        private string _username;
+        private string _password;
+        private string _baseUrl;
+
+        public SharePointFactFetcher(string username, string password, string baseUrl)
+        {
+            _username = username;
+            _password = password;
+            _baseUrl = baseUrl;
+        }
+
+
+        public double GetFileCountByLocation(string libraryID)
+        {
+            var resource = string.Format("/_api/web/lists/getByTitle('{0}')/itemCount", libraryID);
+            var response = ExecuteGet(resource);
+
+            var wrapper = JsonConvert.DeserializeObject<Dictionary<string, object>>(response);
+            var value = JsonConvert.DeserializeObject<Dictionary<string, double>>(wrapper.Values.First().ToString());
+
+            return value["ItemCount"];
+        }
+
+        public IEnumerable<string> GetFilesByLocation(string libraryTitle)
+        {
+            var resource = string.Format("/_api/web/Lists/GetByTitle('{0}')/Items?$expand=Folder&$select=FileLeafRef", libraryTitle);
+            var response = ExecuteGet(resource);
+
+            var wrapper = JsonConvert.DeserializeObject<Dictionary<string, object>>(response);
+            var d = JsonConvert.DeserializeObject<Dictionary<string, object>>(wrapper["d"].ToString());
+            var results = JsonConvert.DeserializeObject<List<SPNameValue>>(d["results"].ToString());
+
+            var filesOut = results.Select(p => p.FileLeafRef);
+
+            //foreach (var item in results.Values)
+            //{
+            //    var value = JsonConvert.DeserializeObject<Dictionary<string, string>>(item);
+            //    filesOut.Add(value["Title"]);
+            //}
+
+            return filesOut;
+        }
+
+        private string ExecuteGet(string resource)
+        {
+            var url = _baseUrl + resource;
+            var endpointRequest = (HttpWebRequest)HttpWebRequest.Create(url);
+
+            endpointRequest.Method = "GET";
+            endpointRequest.Accept = "application/json;odata=verbose";
+            var cred = new System.Net.NetworkCredential(_username, _password);
+            endpointRequest.Credentials = cred;
+            var endpointResponse = (HttpWebResponse)endpointRequest.GetResponse();
+
+            var webResponse = endpointRequest.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+            var responseReader = new StreamReader(webStream);
+            var response = responseReader.ReadToEnd();
+
+            return response;
+        }
+
+    }
+
+    public class SPNameValue
+    {
+        public string FileLeafRef { get; set; }
+    }
+}
